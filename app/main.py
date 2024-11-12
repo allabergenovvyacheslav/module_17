afa4bc2fb523ab627256ad68580a21d8d3eefe27
allabergenovvyacheslav <slawushkin@mail.ru>
2024-11-12 21:08:25 +0300

    Добавил открывашку swagger app = FastAPI(swagger_ui_parameters={"tryItOutEnabled": True})

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 from routers import task, user
 
-app = FastAPI()
+app = FastAPI(swagger_ui_parameters={"tryItOutEnabled": True})
 
 
 @app.get('/')
 async def wellcome():
     return {"message": "Welcome to Taskmanager"}
 
-
+об
 app.include_router(task.router)
 app.include_router(user.router)
